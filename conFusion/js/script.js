2f25e5a0b720a79e4a7455149037bc061143db36
Saloni <suparekh01@gmail.com>
2020-03-21 20:08:46 +0530

    Added script file and changed images

---
+++ conFusion/js/script.js
@@ -0,0 +1,45 @@
+/* 
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+ */
+
+/*Two buttons: pause and play */
+
+/*
+$(document).ready(function(){
+    $('#mycarousel').carousel({interval:2000});
+    $('#carousel-pause').click(function(){
+        $('#mycarousel').carousel('pause');
+    });
+    $('#carousel-play').click(function(){
+        $('#mycarousel').carousel('cycle');
+    });
+});
+*/
+
+/*Single button:pause and play*/
+
+
+$(document).ready(function () {
+    $('#mycarousel').carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+    $("#loginButton").click(function () {
+        $("#loginModal").modal('show');
+    });
+
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal('show');
+    });
+});
